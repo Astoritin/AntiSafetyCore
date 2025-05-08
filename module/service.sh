4b2c726460f9629d113e18435e52c3b590e5fcce
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-08 17:51:33 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -40,32 +40,47 @@ deal_with_app() {
         return 1
     fi
 
+    pkg_basename=$(basename "$pkg_name")
+    pkg_tmp_path="$TMP_DIR/$pkg_basename"
+
+
     if [ "$opt" = "install" ]; then
-        pkg_basename=$(basename "$pkg_name")
-        pkg_tmp_path="$TMP_DIR/$pkg_basename"
+
         cp "$pkg_name" "$TMP_DIR"
+
         logowl "Install package: $pkg_name"
-        logowl "Execute: pm install -i com.android.vending $pkg_tmp_path"
-        su -c "pm install -i com.android.vending $pkg_tmp_path"
+        logowl "Execute: pm install -i com.android.vending $pkg_tmp_path" "TIPS"
+
+        pm install -i "com.android.vending" "$pkg_tmp_path"
+
     elif [ "$opt" = "uninstall" ]; then
+
         logowl "Uninstall package: $pkg_name"
-        logowl "Execute: pm uninstall $pkg_name"
-        su -c "pm uninstall $pkg_name"
+        logowl "Execute: pm uninstall $pkg_name" "TIPS"
+    
+        pm uninstall "$pkg_name"
+    
     fi
 
     result_deal_with_app=$?
     if [ "$result_deal_with_app" -eq 0 ]; then
+
         if [ "$opt" = "install" ]; then
             logowl "$pkg_name has been installed"
             install_count=$((install_count + 1))
+
             rm -f "$pkg_tmp_path"
+
         elif [ "$opt" = "uninstall" ]; then
             logowl "$pkg_name has been slain"
+
             uninstall_count=$((uninstall_count + 1))
+
         fi
         return 0
     else
         logowl "Failed (code: $result_deal_with_app)"
+
         [ "$opt" = "install" ] && rm -f "$pkg_tmp_path"
         return "$result_deal_with_app"
     fi
@@ -76,37 +91,31 @@ install_stub_app()  {
 
     logowl "Install mirror stub APPs"
 
+    desc_sc=""
+    desc_kv=""
+
     deal_with_app "com.google.android.safetycore" "uninstall"
     deal_with_app "com.google.android.contactkeys" "uninstall"
 
-    [ -f "$STUB_DIR/SafetyCoreStub.apk" ] && deal_with_app "$STUB_DIR/SafetyCoreStub.apk" "install"
-    [ -f "$STUB_DIR/SystemKeyVerifierStub.apk" ] && deal_with_app "$STUB_DIR/SystemKeyVerifierStub.apk" "install"
+    [ -f "$STUB_DIR/SafetyCoreStub.apk" ] && deal_with_app "$STUB_DIR/SafetyCoreStub.apk" "install" && desc_sc="✅SafetyCore neutralized."
+    [ -f "$STUB_DIR/SystemKeyVerifierStub.apk" ] && deal_with_app "$STUB_DIR/SystemKeyVerifierStub.apk" "install" && desc_kv="✅KeyVerifier neutralized."
 
-}
-
-module_status_update() {
-
-    logowl "Update module description"
-
-    max_uninstall_count=2
-    max_install_count=2
-
-    DESCRIPTION="A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
-    if [ $install_count -eq $max_install_count ]; then
-        DESCRIPTION="[✅All Done.] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
-    elif [ $install_count -gt 0 ]; then
-        DESCRIPTION="[✅Partially Done.] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
+    if [ -n "$desc_sc" ] && [ -n "$desc_kv" ]; then
+        DESCRIPTION="[✅All Done. $desc_sc $desc_kv] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
+    elif [ -n "$desc_sc" ]; then
+        DESCRIPTION="[✅Done. $desc_sc] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
+    elif [ -n "$desc_kv" ]; then
+        DESCRIPTION="[✅Done. $desc_kv] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
     else
-        DESCRIPTION="[❌No effect. Please check logs!] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
+        DESCRIPTION="[❌No effect. Maybe something went wrong?] A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
     fi
-    
     update_config_value "description" "$DESCRIPTION" "$MODULE_PROP" "true"
+
 }
 
 . "$MODDIR/aautilities.sh"
 
-uninstall_count=0
-install_count=0
+DESCRIPTION="A Magisk module to fight against Google Android System SafetyCore and Android System Key Verifier."
 
 init_logowl "$LOG_DIR"
 module_intro >> "$LOG_FILE"
@@ -117,6 +126,6 @@ while [ "$(getprop sys.boot_completed)" != "1" ]; do
     sleep 5
 done
 logowl "Boot complete!"
-check_module_env && install_stub_app && module_status_update
+check_module_env && install_stub_app
 print_line
-logowl "Service.sh case closed!"
+logowl "service.sh case closed!"
