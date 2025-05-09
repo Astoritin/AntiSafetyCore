040544ef744b249798e105628061dd2057ac13c6
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-09 11:39:34 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -2,13 +2,23 @@
 SKIPUNZIP=1
 
 CONFIG_DIR="/data/adb/antisafetycore"
-STUB_DIR="$CONFIG_DIR/stub"
+PH_DIR="$CONFIG_DIR/placeholder"
 LOG_DIR="$CONFIG_DIR/logs"
 VERIFY_DIR="$TMPDIR/.aa_verify"
 MOD_NAME="$(grep_prop name "$TMPDIR/module.prop")"
 MOD_VER="$(grep_prop version "$TMPDIR/module.prop") ($(grep_prop versionCode "$TMPDIR/module.prop"))"
 
 [ ! -d "$VERIFY_DIR" ] && mkdir -p "$VERIFY_DIR"
+
+
+migrate_old_files() {
+
+    logowl "Migrate old files"
+
+    rm -rf /data/adb/antisafetycore/stub
+    rm -rf /data/adb/antisafetycore/placeholder
+
+}
 
 echo "- Extract aautilities.sh"
 unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
@@ -29,16 +39,17 @@ logowl "Essential check"
 extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
 extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
 clean_old_logs "$LOG_DIR" 20
+migrate_old_files
 logowl "Extract module files"
 extract "$ZIPFILE" 'aautilities.sh' "$MODPATH"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
 extract "$ZIPFILE" 'action.sh' "$MODPATH"
 extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-logowl "Extract stub apks"
-extract "$ZIPFILE" 'stub/SafetyCoreStub.apk' "$STUB_DIR" "true"
-extract "$ZIPFILE" 'stub/SystemKeyVerifierStub.apk' "$STUB_DIR" "true"
-print_line "70"
+logowl "Extract placeholder apks"
+extract "$ZIPFILE" 'placeholder/SafetyCorePlaceHolder.apk' "$PH_DIR" "true"
+extract "$ZIPFILE" 'placeholder/KeyVerifierPlaceHolder.apk' "$PH_DIR" "true"
+print_line "50"
 logowl " "
 logowl "NOTICE & WARNING" "TIPS"
 logowl " "
@@ -46,12 +57,12 @@ logowl "Even though $MOD_NAME will uninstall and reinstall these APPs again auto
 logowl "If you DO NOT want these APPs to come back"
 logowl "Make sure you have DISABLED these options below:"
 logowl " "
-logowl "1. Allow downgrade" "TIPS"
-logowl "2. Disable compare signatures" "TIPS"
+logowl "  1. Allow downgrade" "TIPS"
+logowl "  2. Disable compare signatures" "TIPS"
 logowl " "
 logowl "TIPS: These options are often seen in Xposed module (e.g. Core Patch)"
 logowl " "
-print_line "70"
+print_line "50"
 if [ -n "$VERIFY_DIR" ] && [ -d "$VERIFY_DIR" ] && [ "$VERIFY_DIR" != "/" ]; then
     rm -rf "$VERIFY_DIR"
 fi
