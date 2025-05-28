3504fd41cc34a2e32d975a89aadd0ce198150dfa
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-28 22:43:26 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -12,23 +12,10 @@ MODULE_PROP="$MODDIR/module.prop"
 MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "$MODULE_PROP") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "$MODULE_PROP"))"
-MOD_INTRO="A Magisk module to fight against Android System SafetyCore and Android System Key Verifier installed by Google quietly."
+MOD_INTRO="A Magisk module to fight against Safety Core and Key Verifier installed by Google quietly."
 
 MOD_DESC_OLD="$(sed -n 's/^description=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_ROOT_DIR=$(dirname "$MODDIR")
-
-check_module_env() {
-
-    logowl "Essential check"
-    if [ ! -d "$CONFIG_DIR" ]; then
-        logowl "Config dir $CONFIG_DIR does NOT exist!" "FATAL"
-        return 1
-    elif [ ! -d "$PH_DIR" ]; then
-        logowl "Placeholder dir $PH_DIR does NOT exist!" "FATAL"
-        return 1
-    fi
-
-}
 
 deal_with_app() {
     pkg_name=$1
@@ -39,44 +26,23 @@ deal_with_app() {
     pkg_basename=$(basename "$pkg_name")
     pkg_tmp_path="$TMP_DIR/$pkg_basename"
 
-
     if [ "$opt" = "install" ]; then
-
         cp "$pkg_name" "$TMP_DIR"
-
-        logowl "Install package: $pkg_name"
-        logowl "Execute: pm install -i com.android.vending $pkg_tmp_path" "TIPS"
-
         pm install -i "com.android.vending" "$pkg_tmp_path"
-
     elif [ "$opt" = "uninstall" ]; then
-
-        logowl "Uninstall package: $pkg_name"
-        logowl "Execute: pm uninstall $pkg_name" "TIPS"
-    
         pm uninstall "$pkg_name"
-    
     fi
 
     result_deal_with_app=$?
     if [ "$result_deal_with_app" -eq 0 ]; then
-
         if [ "$opt" = "install" ]; then
-            logowl "$pkg_name has been installed"
             install_count=$((install_count + 1))
-
             rm -f "$pkg_tmp_path"
-
         elif [ "$opt" = "uninstall" ]; then
-            logowl "$pkg_name has been slain"
-
             uninstall_count=$((uninstall_count + 1))
-
         fi
         return 0
     else
-        logowl "Failed (code: $result_deal_with_app)"
-
         [ "$opt" = "install" ] && rm -f "$pkg_tmp_path"
         return "$result_deal_with_app"
     fi
@@ -85,42 +51,36 @@ deal_with_app() {
 
 install_placeholder_app()  {
 
-    logowl "Install placeholder APPs"
-
-    desc_sc=""
-    desc_kv=""
+    ph_sc=""
+    ph_kv=""
     desc_state=""
 
     deal_with_app "com.google.android.safetycore" "uninstall"
     deal_with_app "com.google.android.contactkeys" "uninstall"
 
-    [ -f "$PH_DIR/SafetyCorePlaceHolder.apk" ] && deal_with_app "$PH_DIR/SafetyCorePlaceHolder.apk" "install" && desc_sc="✅SafetyCore neutralized. "
-    [ -f "$PH_DIR/KeyVerifierPlaceHolder.apk" ] && deal_with_app "$PH_DIR/KeyVerifierPlaceHolder.apk" "install" && desc_kv="✅KeyVerifier neutralized."
+    [ -f "$PH_DIR/SafetyCorePlaceHolder.apk" ] && deal_with_app "$PH_DIR/SafetyCorePlaceHolder.apk" "install" && ph_sc="true"
+    [ -f "$PH_DIR/KeyVerifierPlaceHolder.apk" ] && deal_with_app "$PH_DIR/KeyVerifierPlaceHolder.apk" "install" && ph_kv="true"
 
-    if [ -n "$desc_sc" ] && [ -n "$desc_kv" ]; then
-        desc_state="✅All Done."
-    elif [ -n "$desc_sc" ] || [ -n "$desc_kv" ]; then
-        desc_state="✅Done."
+    if [ "$ph_sc" = "true" ] && [ "$ph_kv" = "true" ]; then
+        desc_state="✅All Done. Neutralized: ✅Safety Core, ✅Key Verifier"
+    elif [ "$ph_sc" = "true" ]; then
+        desc_state="✅Done. Neutralized: ✅Safety Core"
+    elif [ "$ph_kv" = "true" ]; then
+        desc_state="✅Done. Neutralized: ✅Key Verifier"
     else
         desc_state="❌No effect. Maybe something went wrong?"
     fi
 
-    DESCRIPTION="[$desc_state ${desc_sc}${desc_kv}] $MOD_INTRO"
+    DESCRIPTION="[$desc_state] $MOD_INTRO"
     update_config_value "description" "$DESCRIPTION" "$MODULE_PROP" "true"
 
 }
 
-. "$MODDIR/aautilities.sh"
+. "$MODDIR/aa-util.sh"
 
-init_logowl "$LOG_DIR"
-module_intro >> "$LOG_FILE"
-show_system_info >> "$LOG_FILE"
-print_line
-logowl "Start service.sh"
+[ ! -d "$CONFIG_DIR" ] && return 1
+[ ! -d "$PH_DIR" ] && return 1
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
     sleep 5
 done
-logowl "Boot complete!"
-check_module_env && install_placeholder_app
-print_line
-logowl "service.sh case closed!"
+install_placeholder_app
