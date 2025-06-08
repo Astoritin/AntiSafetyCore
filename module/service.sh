14eb33981a65c0b138f0e71a981ffa4542f3dd0e
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-08 11:14:33 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -21,58 +21,14 @@ fetch_package_path_from_pm() {
     package_name=$1
     output_pm=$(pm path "$package_name")
 
-    if [ -z "$output_pm" ]; then
-        logowl "Package $package_name is NOT available!" "ERROR"
-        return 1
-    fi
+    [ -z "$output_pm" ] && return 1
+    
     logowl "Output from pm: $output_pm"
 
     package_path=$(echo "$output_pm" | cut -d':' -f2- | sed 's/^://' )
     logowl "Full apk file path of ${package_name}: $package_path"
 
     echo "$package_path"    
-}
-
-fetch_file_size() {
-    file_path=$1
-    output_du=$(du -k "$file_path")
-
-    if [ -z "$output_du" ]; then
-        logowl "File path $file_path is NOT available!" "ERROR"
-        return 1
-    fi
-
-    logowl "Output from du: $output_du"
-
-    file_size=$(echo "$output_du" | grep -o -E '^[0-9]+')
-    logowl "Size of file ${file_path}: $file_size K"
-
-    if [ -n "$file_size" ]; then
-        echo "$file_size"
-        return 0
-    else
-        return 1
-    fi
-
-}
-
-compare_file_size() {
-    file_path_a=$1
-    file_path_b=$2
-
-    [ ! -f "$file_path_a" ] || [ ! -f "$file_path_b" ] && return 1
-
-    size_file_a=$(fetch_file_size "$file_path_a")
-    size_file_b=$(fetch_file_size "$file_path_b")
-
-    if [ "$size_apk_a" -ne "$size_apk_b" ]; then
-        logowl "A != B"
-        return 1
-    else
-        logowl "A == B"
-        return 0
-    fi
-
 }
 
 uninstall_package() {
@@ -108,7 +64,9 @@ install_package() {
 
 }
 
-install_placeholder_app()  {
+install_placeholder_app() {
+
+    logowl "Start installing placeholder app"
 
     PN_SC="com.google.android.safetycore"
     PN_KV="com.google.android.contactkeys"
@@ -123,7 +81,7 @@ install_placeholder_app()  {
     replaced_kv="false"
     desc_state=""
 
-    if compare_file_size "$PATH_C_SC" "$PATH_PH_SC"; then
+    if file_compare "$PATH_C_SC" "$PATH_PH_SC"; then
         logowl "No needs to reintall SafetyCore PlaceHolder APP"
         replaced_sc="true"
     else
@@ -132,7 +90,7 @@ install_placeholder_app()  {
         [ -f "$PATH_PH_SC" ] && install_package "$PATH_PH_SC" && replaced_sc="true"
     fi
 
-    if compare_file_size "$PATH_C_KV" "$PATH_PH_KV"; then
+    if file_compare "$PATH_C_KV" "$PATH_PH_KV"; then
         logowl "No needs to reintall KeyVerifier PlaceHolder APP"
         replaced_kv="true"
     else
@@ -165,8 +123,9 @@ module_intro >> "$LOG_FILE"
 show_system_info >> "$LOG_FILE"
 print_line
 logowl "Start service.sh"
-print_line
 logowl "Wait for boot complete"
+print_line
+
 boot_count=0
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
     boot_count=$((boot_count + 1))
@@ -176,3 +135,5 @@ done
 logowl "Boot completed after ${boot_count}s!"
 install_placeholder_app
 logowl_clean "$LOG_DIR" 20
+print_line
+logowl "service.sh case closed!"
