f4175c5e3ad7687e1f6111f2472fd09d8eaf3abe
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-22 19:34:25 +0800

    Update aa-util.sh

--- module/aa-util.sh
+++ module/aa-util.sh
@@ -70,44 +70,6 @@ install_env_check() {
 
 }
 
-module_intro() {
-
-    install_env_check
-    print_line
-    logowl "$MOD_NAME"
-    logowl "By $MOD_AUTHOR"
-    logowl "Version: $MOD_VER"
-    logowl "Root: $ROOT_SOL_DETAIL"
-    logowl "Timestamp: $(date +"%Y-%m-%d %H:%M:%S")"
-    logowl "Module dir: $MODDIR"
-    print_line
-
-}
-
-logowl_init() {
-    LOG_DIR="$1"
-
-    [ -z "$LOG_DIR" ] && return 1
-    [ ! -d "$LOG_DIR" ] && mkdir -p "$LOG_DIR" && logowl "Created $LOG_DIR"
-
-}
-
-logowl_clean() {
-    log_dir="$1"
-    files_max="$2"
-    
-    [ -z "$log_dir" ] || [ ! -d "$log_dir" ] && return 1
-    [ -z "$files_max" ] && files_max=30
-
-    files_count=$(ls -1 "$log_dir" | wc -l)
-    if [ "$files_count" -gt "$files_max" ]; then
-        ls -1t "$log_dir" | tail -n +$((files_max + 1)) | while read -r file; do
-            rm -f "$log_dir/$file"
-        done
-    fi
-    return 0
-}
-
 logowl() {
     LOG_MSG="$1"
     LOG_MSG_LEVEL="$2"
@@ -116,10 +78,11 @@ logowl() {
     [ -z "$LOG_MSG" ] && return 1
 
     case "$LOG_MSG_LEVEL" in
-        "TIPS") LOG_MSG_PREFIX="> " ;;
-        "WARN") LOG_MSG_PREFIX="- Warn: " ;;
-        "ERROR") LOG_MSG_PREFIX="! ERROR: " ;;
-        "FATAL") LOG_MSG_PREFIX="× FATAL: " ;;
+        "W") LOG_MSG_PREFIX="? Warn: " ;;
+        "E") LOG_MSG_PREFIX="! ERROR: " ;;
+        "F") LOG_MSG_PREFIX="× FATAL: " ;;
+        ">") LOG_MSG_PREFIX="> " ;;
+        "*" ) LOG_MSG_PREFIX="* " ;; 
         " ") LOG_MSG_PREFIX="  " ;;
         "-") LOG_MSG_PREFIX="" ;;
         *) LOG_MSG_PREFIX="- " ;;
@@ -152,42 +115,20 @@ logowl() {
     fi
 }
 
-print_line() {
-
-    length=${1:-51}
-    symbol=${2:--}
-
-    line=$(printf "%-${length}s" | tr ' ' "$symbol")
-    logowl "$line" "-"
-
-}
-
 update_config_var() {
     key_name="$1"
     key_value="$2"
     file_path="$3"
 
-    [ -z "$key_name" ] || [ -z "$key_value" ] || [ -z "$file_path" ] && return 1
-    [ ! -f "$file_path" ] && return 2
+    if [ -z "$key_name" ] || [ -z "$key_value" ] || [ -z "$file_path" ]; then
+        return 1
+    elif [ ! -f "$file_path" ]; then
+        return 2
+    fi
 
     sed -i "/^${key_name}=/c\\${key_name}=${key_value}" "$file_path"
-
     result_update_value=$?
     return "$result_update_value"
-
-}
-
-debug_print_values() {
-
-    print_line
-    logowl "All Environment variables"
-    print_line
-    env | sed 's/^/- /'
-    print_line
-    logowl "All Shell variables"
-    print_line
-    ( set -o posix; set ) | sed 's/^/- /'
-    print_line
 
 }
 
@@ -195,28 +136,14 @@ show_system_info() {
 
     logowl "Device: $(getprop ro.product.brand) $(getprop ro.product.model) ($(getprop ro.product.device))"
     logowl "OS: Android $(getprop ro.build.version.release) (API $(getprop ro.build.version.sdk)), $(getprop ro.product.cpu.abi | cut -d '-' -f1)"
-
-}
-
-file_compare() {
-    file_a="$1"
-    file_b="$2"
-    
-    [ -z "$file_a" ] || [ -z "$file_b" ] && return 2
-    [ ! -f "$file_a" ] || [ ! -f "$file_b" ] && return 3
-    
-    hash_file_a=$(sha256sum "$file_a" | awk '{print $1}')
-    hash_file_b=$(sha256sum "$file_b" | awk '{print $1}')
-    
-    [ "$hash_file_a" = "$hash_file_b" ] && return 0
-    [ "$hash_file_a" != "$hash_file_b" ] && return 1
+    logowl "Kernel: $(uname -r)"
 
 }
 
 abort_verify() {
 
     [ -n "$VERIFY_DIR" ] && [ -d "$VERIFY_DIR" ] && [ "$VERIFY_DIR" != "/" ] && rm -rf "$VERIFY_DIR"
-    logowl "$1" "ERROR"
+    logowl "$1" "E"
     abort "This zip may be corrupted or has been maliciously modified!"
 
 }
@@ -241,11 +168,10 @@ extract() {
     fi
 
     unzip $opts "$zip" "$file" -d "$dir" >&2
-    [ -f "$file_path" ] || abort_verify "$file does NOT exist!"
-    logowl "Extract $file → $file_path" >&1
+    [ -f "$file_path" ] || abort_verify "$file does NOT exist"
 
     unzip $opts "$zip" "$file.sha256" -d "$VERIFY_DIR" >&2
-    [ -f "$hash_path" ] || abort_verify "$file.sha256 does NOT exist!"
+    [ -f "$hash_path" ] || abort_verify "$file.sha256 does NOT exist"
 
     expected_hash="$(cat "$hash_path")"
     calculated_hash="$(sha256sum "$file_path" | cut -d ' ' -f1)"
