9ba8ffcb9922708155dcf360a191b2fa0871dd9c
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-22 19:35:03 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -2,31 +2,20 @@
 MODDIR=${0%/*}
 
 CONFIG_DIR="/data/adb/antisafetycore"
-LOG_DIR="$CONFIG_DIR/logs"
-LOG_FILE="$LOG_DIR/asc_core_$(date +"%Y%m%dT%H%M%S").log"
 
 PH_DIR="$CONFIG_DIR/placeholder"
 TMP_DIR="/data/local/tmp"
 
 MODULE_PROP="$MODDIR/module.prop"
-MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "$MODULE_PROP")"
-MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "$MODULE_PROP")"
-MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "$MODULE_PROP") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "$MODULE_PROP"))"
-MOD_INTRO="Fight against SafetyCore and KeyVerifier installed by Google quietly."
-
-MOD_DESC_OLD="$(sed -n 's/^description=\(.*\)/\1/p' "$MODULE_PROP")"
-MOD_ROOT_DIR=$(dirname "$MODDIR")
+MOD_INTRO="Fight against SafetyCore and KeyVerifier."
 
 fetch_package_path_from_pm() {
     package_name=$1
     output_pm=$(pm path "$package_name")
 
     [ -z "$output_pm" ] && return 1
-    
-    logowl "Output from pm: $output_pm"
 
     package_path=$(echo "$output_pm" | cut -d':' -f2- | sed 's/^://' )
-    logowl "Full apk file path of ${package_name}: $package_path"
 
     echo "$package_path"    
 }
@@ -35,7 +24,6 @@ uninstall_package() {
 
     package_name="$1"
 
-    logowl "Execute: pm uninstall $package_name"
     pm uninstall "$package_name"
     result_uninstall_package=$?
 
@@ -47,26 +35,52 @@ install_package() {
 
     package_path="$1"
 
-    logowl "Execute: cp $package_path $TMP_DIR"
-
     cp "$package_path" "$TMP_DIR"
 
     package_basename=$(basename "$package_path")
     package_tmp="$TMP_DIR/$package_basename"
 
-    logowl "Execute: pm install -i com.android.vending $package_tmp"
     pm install -i "com.android.vending" "$package_tmp"
     result_install_package=$?
 
-    logowl "Execute: rm -f $package_tmp"
     rm -f "$package_tmp"
     return "$result_install_package"    
 
 }
 
-install_placeholder_app() {
+update_config_var() {
+    key_name="$1"
+    key_value="$2"
+    file_path="$3"
 
-    logowl "Start installing placeholder app"
+    if [ -z "$key_name" ] || [ -z "$key_value" ] || [ -z "$file_path" ]; then
+        return 1
+    elif [ ! -f "$file_path" ]; then
+        return 2
+    fi
+
+    sed -i "/^${key_name}=/c\\${key_name}=${key_value}" "$file_path"
+    result_update_value=$?
+    return "$result_update_value"
+
+}
+
+file_compare() {
+    file_a="$1"
+    file_b="$2"
+    
+    [ -z "$file_a" ] || [ -z "$file_b" ] && return 2
+    [ ! -f "$file_a" ] || [ ! -f "$file_b" ] && return 3
+    
+    hash_file_a=$(sha256sum "$file_a" | awk '{print $1}')
+    hash_file_b=$(sha256sum "$file_b" | awk '{print $1}')
+    
+    [ "$hash_file_a" = "$hash_file_b" ] && return 0
+    [ "$hash_file_a" != "$hash_file_b" ] && return 1
+
+}
+
+install_placeholder_app() {
 
     PN_SC="com.google.android.safetycore"
     PN_KV="com.google.android.contactkeys"
@@ -82,7 +96,6 @@ install_placeholder_app() {
     desc_state=""
 
     if file_compare "$PATH_C_SC" "$PATH_PH_SC"; then
-        logowl "No needs to reintall SafetyCore PlaceHolder APP"
         replaced_sc="true"
     else
         replaced_sc="false"
@@ -91,7 +104,6 @@ install_placeholder_app() {
     fi
 
     if file_compare "$PATH_C_KV" "$PATH_PH_KV"; then
-        logowl "No needs to reintall KeyVerifier PlaceHolder APP"
         replaced_kv="true"
     else
         replaced_kv="false"
@@ -100,13 +112,13 @@ install_placeholder_app() {
     fi
 
     if [ "$replaced_sc" = "true" ] && [ "$replaced_kv" = "true" ]; then
-        desc_state="✅All Done. Neutralized: ✅SafetyCore, ✅KeyVerifier"
+        desc_state="✅Cleared. Slain: ✅SafetyCore, ✅KeyVerifier"
     elif [ "$replaced_sc" = "true" ]; then
-        desc_state="✅Done. Neutralized: ✅SafetyCore"
+        desc_state="✅Done. Slain: ✅SafetyCore"
     elif [ "$replaced_kv" = "true" ]; then
-        desc_state="✅Done. Neutralized: ✅KeyVerifier"
+        desc_state="✅Done. Slain: ✅KeyVerifier"
     else
-        desc_state="❌No effect. Maybe something went wrong?"
+        desc_state="❌No effect. Something went wrong!"
     fi
 
     DESCRIPTION="[$desc_state] $MOD_INTRO"
@@ -116,24 +128,10 @@ install_placeholder_app() {
 
 [ ! -d "$PH_DIR" ] && return 1
 
-. "$MODDIR/aa-util.sh"
-
-logowl_init "$LOG_DIR"
-module_intro >> "$LOG_FILE"
-show_system_info >> "$LOG_FILE"
-print_line
-logowl "Start service.sh"
-logowl "Wait for boot complete"
-print_line
-
 boot_count=0
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
     boot_count=$((boot_count + 1))
     sleep 1
 done
 
-logowl "Boot completed after ${boot_count}s!"
 install_placeholder_app
-logowl_clean "$LOG_DIR" 20
-print_line
-logowl "service.sh case closed!"
