d23ec9cd6a8f60ab06d951019d0548e20c8d89e9
Astoritin <Astoritin@outlook.com>
2025-11-27 23:47:49 +0800

    Add auto uninstall script as uninstalling module This may fix the issue while placeholder apps still exist after executing uninstall module

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,3 +1,16 @@
 #!/system/bin/sh
 
 rm -rf "/data/adb/anti_safetycore"
+
+[ ! -d "/data/adb/service.d/" ] && mkdir -p "/data/adb/service.d"
+
+cat > "/data/adb/service.d/uninstall_anti_safetycore.sh" << 'EOF'
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+    sleep 1
+done
+pm uninstall com.google.android.safetycore
+pm uninstall com.google.android.contactkeys
+rm -f "/data/adb/service.d/uninstall_anti_safetycore.sh"
+EOF
+
+chmod +x "/data/adb/service.d/uninstall_anti_safetycore.sh"
