a695078465c7703e62d65cedaca8e845ecf60441
Astoritin <Astoritin@outlook.com>
2026-01-28 09:31:13 +0800

    opt: add root solutions detecting functions

--- module/service.sh
+++ module/service.sh
@@ -17,6 +17,71 @@ replaced_sc="false"
 replaced_kv="false"
 desc_state=""
 
+is_magisk() {
+
+    if ! command -v magisk >/dev/null 2>&1; then
+        return 1
+    fi
+
+    MAGISK_V_VER_NAME="$(magisk -v)"
+    MAGISK_V_VER_CODE="$(magisk -V)"
+    case "$MAGISK_V_VER_NAME" in
+        *"-alpha"*) MAGISK_BRANCH_NAME="Alpha" ;;
+        *) MAGISK_BRANCH_NAME="Magisk" ;;
+    esac
+    DETECT_MAGISK="true"
+    return 0
+
+}
+
+is_kernelsu() {
+    if [ -n "$KSU" ]; then
+        DETECT_KSU="true"
+        ROOT_SOL="KernelSU"
+        return 0
+    fi
+    return 1
+}
+
+is_apatch() {
+    if [ -n "$APATCH" ]; then
+        DETECT_APATCH="true"
+        ROOT_SOL="APatch"
+        return 0
+    fi
+    return 1
+}
+
+install_env_check() {
+
+    ROOT_SOL="Magisk"
+    ROOT_SOL_COUNT=0
+
+    is_kernelsu && ROOT_SOL_COUNT=$((ROOT_SOL_COUNT + 1))
+    is_apatch && ROOT_SOL_COUNT=$((ROOT_SOL_COUNT + 1))
+    is_magisk && ROOT_SOL_COUNT=$((ROOT_SOL_COUNT + 1))
+
+    if [ "$DETECT_KSU" = "true" ]; then
+        ROOT_SOL="KernelSU"
+        ROOT_SOL_DETAIL="KernelSU ($KSU_KERNEL_VER_CODE)"
+    elif [ "$DETECT_APATCH" = "true" ]; then
+        ROOT_SOL="APatch"
+        ROOT_SOL_DETAIL="APatch ($APATCH_VER_CODE)"
+    elif [ "$DETECT_MAGISK" = "true" ]; then
+        ROOT_SOL="Magisk"
+        ROOT_SOL_DETAIL="$MAGISK_BRANCH_NAME (${MAGISK_VER_CODE:-$MAGISK_V_VER_CODE})"
+    fi
+
+    if [ "$ROOT_SOL_COUNT" -gt 1 ]; then
+        ROOT_SOL="Multiple"
+        ROOT_SOL_DETAIL="Multiple"
+    elif [ "$ROOT_SOL_COUNT" -lt 1 ]; then
+        ROOT_SOL="Unknown"
+        ROOT_SOL_DETAIL="Unknown"
+    fi
+
+}
+
 check_data_encrypted() { [ "$data_state" = "encrypted" ]; }
 
 file_compare() {
