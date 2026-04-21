d6741ce5f538621b50af5f148221442a03b3409a
Astoritin <Astoritin@outlook.com>
2026-04-21 22:08:50 +0800

    fix: remove unused function

--- module/cleanup_anti_safetycore.sh
+++ module/cleanup_anti_safetycore.sh
@@ -4,8 +4,6 @@ module_dir="/data/adb/modules/anti_safetycore"
 module_update_dir="/data/adb/modules_update/anti_safetycore"
 module_description="GET LOST, SafetyCore & KeyVerifier!"
 
-update_description() { [ -n "$1" ] || return 1; ; }
-
 [ -f "$module_dir/disable" ] && sed -i "s/^description=.*/description=${module_description}/" "$module_dir/module.prop"
 
 if [ -f "/data/adb/anti_safetycore/systemize" ]; then
